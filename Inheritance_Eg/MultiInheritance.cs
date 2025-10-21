8b996ad5a994a91e22955967a7d517bac50cb1ee
Adarsh <adarshkannanvfc@gmail.com>
2025-10-21 11:23:41 +0530

    Add project files.

---
+++ Inheritance_Eg/MultiInheritance.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Eg
+{
+    internal class Digits
+    {
+        protected int num1, num2;
+        public void ReadDigits()
+        {
+            Console.Write("Enter num1: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter num2: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+
+    internal class  Calculate : Digits 
+    {
+        protected int sum = 0;
+        public void FindSum()
+        {
+            int numb1 = num1;
+            int numb2 = num2;
+            sum = numb1 + numb2;
+        }
+    }
+
+    internal class DisplayResult : Calculate
+    {
+        public void Show()
+        {
+            Console.WriteLine($"sum is : {sum}");
+        }
+    }
+}
---
+++ Inheritance_Eg/Program.cs
@@ -0,0 +1,14 @@
+﻿using Inheritance_Eg;
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DisplayResult dr = new DisplayResult();
+        dr.ReadDigits();
+        dr.FindSum();
+        dr.Show();
+    }
+}
