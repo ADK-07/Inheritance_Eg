9e1eb52b746d56f807db7d842b7bc3ae50d19ca1
Adarsh <adarshkannanvfc@gmail.com>
2025-10-22 11:37:44 +0530

    hierachial inheritance example

--- Inheritance_Eg/HierarchialInheritance.cs
+++ Inheritance_Eg/HierarchialInheritance.cs
@@ -2,11 +2,56 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace Inheritance_Eg
 {
-    internal class HierarchialInheritance
+    internal class ReadNumber
     {
+        protected int[] numArray;
+        public void ReadNum(int size)
+        {
+            numArray = new int[size];  
+            for( int i = 0; i < size; i++)
+            {
+                numArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        public int[] GetNumber()
+        {
+            return numArray;
+        }
+
+        public void SetNumber(int[] number)
+        {
+            numArray = number;
+        }
+    }
+
+    internal class Sum : ReadNumber
+    {
+        public int FindSum()
+        {
+            int sum = 0;
+            foreach (int i in numArray)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+    }
+
+    internal class Avg : ReadNumber
+    {
+        public double FindAvg()
+        {
+            int sum = 0;
+            foreach (int i in numArray)
+            {
+                sum += i;   
+            }
+            return (double)sum / numArray.Length;
+        }
     }
 }
--- Inheritance_Eg/Program.cs
+++ Inheritance_Eg/Program.cs
@@ -1,14 +1,33 @@
 ﻿using Inheritance_Eg;
-using System;
-using System.Security.Cryptography.X509Certificates;
+using System.Threading.Channels;
 
 class Program
 {
     static void Main(string[] args)
     {
-        DisplayResult dr = new DisplayResult();
-        dr.ReadDigits();
-        dr.FindSum();
-        dr.Show();
+        //------- Multiple inheritance --------
+
+        //DisplayResult dr = new DisplayResult();
+        //dr.ReadDigits();
+        //dr.FindSum();
+        //dr.Show();
+
+
+        //--------hierarchialinheritance-----------
+
+        //ReadNumber rn = new ReadNumber();
+        //Console.Write("Enter the size of array: ");
+        //int n = Convert.ToInt32(Console.ReadLine());
+        //rn.ReadNum(n);
+
+        //Sum s = new Sum();
+        //s.SetNumber(rn.GetNumber());
+        //s.FindSum();
+        //Console.WriteLine("Sum is : " + s.FindSum());
+
+        //Avg a = new Avg();
+        //a.SetNumber(rn.GetNumber());
+        //a.FindAvg();
+        //Console.WriteLine("Avg is :" + a.FindAvg());
     }
 }
